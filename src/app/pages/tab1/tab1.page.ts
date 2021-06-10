a2d251abdb7fa7c2d264cae2cfae6dd45e57e11d
HectorMG <hector980715@gmail.com>
2021-06-09 21:18:37 -0500

    Navegacion entre pantallas

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { Lista } from 'src/app/models/lista.model';
 import { DeseosService } from 'src/app/services/deseos.service';
 
@@ -11,8 +12,12 @@ export class Tab1Page {
 
   listas: Lista[] = [];
 
-  constructor(private deseoService: DeseosService) {
+  constructor(private deseoService: DeseosService, private router: Router) {
     this.listas = this.deseoService.getListas();
+  }
+
+  agregar(){
+    this.router.navigate(['/tabs/tab1/agregar']);
   }
 
 }
